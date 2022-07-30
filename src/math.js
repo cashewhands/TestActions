375923e21d054922d1e39cd5ece0d3c76857b178
Cashewhands <demoladev.ops@gmail.com>
2022-07-30 02:20:54 +0100

    CHGX: Trial

--- src/math.js
+++ src/math.js
@@ -1,23 +1,23 @@
-// const  sum = (a, b) => {
-//     return a + b;
-// }
+const  sum = (a, b) => {
+    return a + b;
+}
 
-// const substract = (a, b) => {
-//     return a - b;
-// }
+const substract = (a, b) => {
+    return a - b;
+}
 
-// const multiply = (a, b) => {
-//     return a * b;
-// }
+const multiply = (a, b) => {
+    return a * b;
+}
 
-// const divide = (a, b) => {
-//     return a / b;
-// }
+const divide = (a, b) => {
+    return a / b;
+}
 
 
-// module.exports = {
-//     sum,
-//     substract,
-//     multiply,
-//     divide
-// }
+module.exports = {
+    sum,
+    substract,
+    multiply,
+    divide
+}
