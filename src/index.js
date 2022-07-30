bf99fc664dc92622ba257462166c7ee27111464d
Cashewhands <demoladev.ops@gmail.com>
2022-07-30 02:38:11 +0100

    DOC-missing somethimg

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.get('/', function(req,res) {
 })
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(3000, async function() {
     var host = server.address().address;
     var port = server.address().port;
 
