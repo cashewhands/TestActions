16bf4fd5389e8eb5e77980424be259d34a64ff8a
Cashewhands <demoladev.ops@gmail.com>
2022-07-30 20:49:40 +0100

    Chore: CHGX to server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-let express = require('express')
-let app = express();
+const express = require('express')
+const app = express();
+const cors = require("cors")
 
+
+app.use(cors());
 app.disable("x-powered-by");
 
+// Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 // let helmet = require("helmet");
 // let app2 = express(); // Compliant
 // app2.use(helmet.hidePoweredBy());
-app.get('/', function(req,res) {
-    console.log("This was added to make it look good!")
-    res.send('Hello World!');
-})
 
 
-var server = app.listen(3000, async function() {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(PORT, async function() {
+    const host = server.address().address;
+    const port = server.address().port;
+    
+    console.log('Server listening at http://127.0.0.1:%s', port);
+});
 
-    console.log('Example app listening at http://%s:%s', host, port);
-})
+app.get('/', (req,res) => res.send('API Running!'));
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
+
+  module.exports = app;
